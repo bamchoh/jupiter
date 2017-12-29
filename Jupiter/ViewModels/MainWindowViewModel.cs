d5d4adbee2151b3338934a37e776841574f39a2d
bamchoh <bamchoh@gmail.com>
2017-12-29 19:36:37 +0900

    Fix for #1

--- Jupiter/ViewModels/MainWindowViewModel.cs
+++ Jupiter/ViewModels/MainWindowViewModel.cs
@@ -13,34 +13,14 @@ using Prism.Mvvm;
 
 namespace Jupiter.ViewModels
 {
-    public class MainWindowViewModel : BindableBase
+    public class MainWindowViewModel
     {
         #region Private Fields
         private Interfaces.IConnection client;
         #endregion
 
-        #region Properties
-        private object connectionViewModel;
-        public object ConnectionViewModel
-        {
-            get
-            {
-                return connectionViewModel;
-            }
-
-            private set
-            {
-                this.SetProperty(ref connectionViewModel, value);
-            }
-        }
-
-        #endregion
-
         #region Commands
-        public ICommand ShowMessageCommand { get; set; }
-
         public ICommand ClosingCommand { get; set; }
-
         #endregion
 
         #region Constructor
@@ -49,36 +29,10 @@ namespace Jupiter.ViewModels
             this.client = client;
 
             ClosingCommand = new Commands.DelegateCommand(
-                (param) => { /* Close(); */ },
+                (param) => { client.Close(); },
                 (param) => true);
-
-            // this.ConnectionViewModel = new ViewModels.ConnectionViewModel(client, ShowMessageCommand, references);
         }
 
-        #endregion
-
-        #region Private Methods
-
-        private void SelectDeletedLastIndex(IList<VariableInfoBase> items, int i)
-        {
-            if (items.Count == 0)
-                return;
-
-            if (i < items.Count)
-            {
-                items[i].IsSelected = true;
-            }
-            else
-            {
-                var j = items.Count - 1;
-                items[j].IsSelected = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            /* Close(); */
-        }
         #endregion
     }
 
