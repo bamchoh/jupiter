cb93dadb2deed8d1fb55ee2399d1694cef95ae08
bamchoh <bamchoh@gmail.com>
2018-11-11 02:12:01 +0900

    Enable CanExecute of Delete Monitored Item Button

--- Jupiter/Views/SubscriptionView.xaml.cs
+++ Jupiter/Views/SubscriptionView.xaml.cs
@@ -23,31 +23,9 @@ namespace Jupiter.Views
     /// </summary>
     public partial class SubscriptionView : UserControl
     {
-        public static DependencyProperty DeleteMonitoredItemsCommandProperty =
-            DependencyProperty.Register(
-                "DeleteMonitoredItemsCommand",
-                typeof(ICommand),
-                typeof(SubscriptionView));
-
-        public ICommand DeleteMonitoredItemsCommand
-        {
-            get { return (ICommand)GetValue(DeleteMonitoredItemsCommandProperty); }
-            set { SetValue(DeleteMonitoredItemsCommandProperty, value); }
-        }
-
         public SubscriptionView()
         {
             InitializeComponent();
-
-            DeleteMonitoredItemsCommand = new DelegateCommand(
-                (param) => { return; },
-                (param) => false);
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            if(DeleteMonitoredItemsCommand.CanExecute(null))
-                DeleteMonitoredItemsCommand.Execute(null);
         }
     }
 }
