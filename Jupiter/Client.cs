de7c7e553ddcd2a0bd71445340416d6d4b2b8ab9
bamchoh <bamchoh@gmail.com>
2020-08-20 01:52:25 +0900

    Refactoring

--- Jupiter/Client.cs
+++ Jupiter/Client.cs
@@ -33,38 +33,6 @@ using System.Security.Policy;
 
 namespace Jupiter
 {
-    public class ServerAndEndpointsPair
-    {
-        public string DiscoveryURL { get; }
-        public string ApplicationName { get; }
-        public EndpointDescriptionCollection Endpoints { get; set; }
-
-        public ServerAndEndpointsPair(string discoveryURL, string applicationName)
-        {
-            DiscoveryURL = discoveryURL;
-            ApplicationName = applicationName;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0} - {1}", ApplicationName, DiscoveryURL);
-        }
-
-        public static List<string> SecurityList(List<ServerAndEndpointsPair> servers, int selectedIndex)
-        {
-            var list = new List<string>();
-
-            foreach (var endpoint in servers[selectedIndex].Endpoints)
-            {
-                list.Add(string.Format("{0} - {1}",
-                    Opc.Ua.SecurityPolicies.GetDisplayName(endpoint.SecurityPolicyUri),
-                    endpoint.SecurityMode));
-            }
-
-            return list;
-        }
-    }
-
     public class Client : BindableBase, Interfaces.IConnection, Interfaces.IReferenceFetchable, Interfaces.INodeInfoGetter, Interfaces.ISubscriptionOperatable, Interfaces.IOneTimeAccessOperator
     {
         public IEventAggregator EventAggregator { get; set; }
---
+++ Jupiter/ServerAndEndpointsPair.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Opc.Ua;
+
+namespace Jupiter
+{
+    public class ServerAndEndpointsPair
+    {
+        public string DiscoveryURL { get; }
+        public string ApplicationName { get; }
+        public EndpointDescriptionCollection Endpoints { get; set; }
+
+        public ServerAndEndpointsPair(string discoveryURL, string applicationName)
+        {
+            DiscoveryURL = discoveryURL;
+            ApplicationName = applicationName;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1}", ApplicationName, DiscoveryURL);
+        }
+
+        public static List<string> SecurityList(List<ServerAndEndpointsPair> servers, int selectedIndex)
+        {
+            var list = new List<string>();
+
+            foreach (var endpoint in servers[selectedIndex].Endpoints)
+            {
+                list.Add(string.Format("{0} - {1}",
+                    Opc.Ua.SecurityPolicies.GetDisplayName(endpoint.SecurityPolicyUri),
+                    endpoint.SecurityMode));
+            }
+
+            return list;
+        }
+    }
+}
