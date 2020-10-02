ad6cef45f371bd4926e4722266611c768c09ff7b
bamchoh <bamchoh@gmail.com>
2020-10-03 03:28:30 +0900

    Update version to 0.1.0.19

--- Jupiter/Properties/AssemblyInfo.cs
+++ Jupiter/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.18")]
-[assembly: AssemblyFileVersion("0.1.0.18")]
-[assembly: AssemblyInformationalVersion("0.1.0.18")]
+[assembly: AssemblyVersion("0.1.0.19")]
+[assembly: AssemblyFileVersion("0.1.0.19")]
+[assembly: AssemblyInformationalVersion("0.1.0.19")]
